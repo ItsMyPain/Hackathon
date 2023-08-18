97ae2003e844c36bdea63ac45d16095562f00c80
Sergey <agibalov1294@gmail.com>
2023-08-18 13:34:09 +0300

    updated admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from .forms import RegistrationForm
 from .models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'email']
+    list_display = ['first_name', 'last_name', 'email', 'created_at', 'updated_at']
+    form = RegistrationForm
--- shop/models.py
+++ shop/models.py
@@ -11,8 +11,12 @@ class User(models.Model):
 
     state_id = models.IntegerField(default=1)
 
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время регистрации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Последнее обновление')
 
     def __str__(self):
         return f"{self.first_name}, {self.last_name}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
